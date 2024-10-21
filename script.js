f7bcec43ee8271de27fd3d9c5f03510d87ece9f8
melvynonline <melvyn@melvynonline.com>
2024-10-21 19:40:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-const links = document.querySelectorAll('.nav-links li'); // Select all the links
 
-// Toggle the menu when the hamburger is clicked
+// Toggle the menu and dark background when the hamburger is clicked
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
-// Close the menu when a link is clicked
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.classList.remove('active');  // Remove the active class to hide the menu
-    });
+    navLinks.classList.toggle('active');   // Toggle the menu visibility
+    hamburger.classList.toggle('active');  // Toggle the dark background for the hamburger
 });
